a4f28468fb97d65c1e7bc63ce645f589e99e3764
Shuhei Gotou <uhsog.tsukuba@gmail.com>
2017-07-11 14:54:49 +0900

    get all problems

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,8 +15,25 @@ module.exports = {
   plugins: [
     'html'
   ],
+  // check if imports actually resolve
+  'settings': {
+    'import/resolver': {
+      'webpack': {
+        'config': 'build/webpack.base.conf.js'
+      }
+    }
+  },
   // add your custom rules here
   'rules': {
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      'js': 'never',
+      'vue': 'never'
+    }],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': ['error', {
+      'optionalDependencies': ['test/unit/index.js']
+    }],
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
--- src/components/AllProblems.vue
+++ src/components/AllProblems.vue
@@ -1,15 +1,37 @@
 <template>
+  <div class="">
     <h1>困りごとの一覧ページ</h1>
-    <!-- <p>{{ msg }}</p>     -->
+  <!-- <p>{{ msg }}</p>     -->
+    <ul class="card-list">
+      <li v-for="problem in problems">
+        <!-- <problem-card :problem="problem" class="card"></problem-card> -->
+        <p>{{ problem.comment }}</p>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+import { WEB_API_URL, TOKEN } from './../../.env';
+
 export default {
   name: 'all-problems',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      problems: '',
     };
+  },
+  created() {
+    const config = {
+      headers: { Authorization: TOKEN },
+    };
+    axios.get(`${WEB_API_URL}/v1/problems`, config)
+            .then((response) => {
+              this.problems = response.data;
+              console.log(this.problems);
+            }).catch(() => {
+            });
   },
 };
 </script>
