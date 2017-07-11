a2e3845de8d4e32fa2491c09868f12000c6be29d
Shuhei Gotou <uhsog.tsukuba@gmail.com>
2017-07-11 13:54:50 +0900

    set eslint config airbnb style

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
     browser: true,
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'airbnb-based',
+  extends: 'airbnb-base',
   // required to lint *.vue files
   plugins: [
     'html'
