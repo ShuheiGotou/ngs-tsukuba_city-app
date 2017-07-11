136cfd7bc3afb5599b979eb35ba061c6d10b1d35
Shuhei Gotou <uhsog.tsukuba@gmail.com>
2017-07-11 14:10:37 +0900

    add new page for show problems

---
+++ src/components/AllProblems.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>困りごとの一覧ページ</h1>
+    <!-- <p>{{ msg }}</p>     -->
+</template>
+
+<script>
+export default {
+  name: 'all-problems',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<style lang="css">
+</style>
