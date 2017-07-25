946aee3faaf949c2bba88add394d99be2216d056
Shuhei Gotou <uhsog.tsukuba@gmail.com>
2017-07-25 16:18:20 +0900

    add login page

---
+++ src/components/Login.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="">
+    <p>login page</p>
+    <el-row>
+      <el-col :span='12' :offset='6'>
+        <el-card class="box-card">
+          <el-form :label-position="top" label-width="100px" :model="formData">
+            <el-form-item label="Email">
+              <el-input v-model="formData.name"></el-input>
+            </el-form-item>
+            <el-form-item label="Pass">
+              <el-input v-model="formData.passward"></el-input>
+            </el-form-item>
+            <el-form-item label="">
+              <el-button type="primary">send</el-button>
+            </el-form-item>
+          </el-form>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data() {
+    return {
+      formData: {
+        email: '',
+        passward: '',
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+.box-card {
+  margin-bottom: 20px;
+}
+.input {
+  width:50%;
+}
+</style>
