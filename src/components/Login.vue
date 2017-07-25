66d2c8cb23c3065fee5c390141b77c51070308f4
Shuhei Gotou <uhsog.tsukuba@gmail.com>
2017-07-25 16:55:29 +0900

    get access token by login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,15 +4,15 @@
     <el-row>
       <el-col :span='12' :offset='6'>
         <el-card class="box-card">
-          <el-form :label-position="top" label-width="100px" :model="formData">
+          <el-form label-form="top" label-width="100px" :model="formData">
             <el-form-item label="Email">
-              <el-input v-model="formData.name"></el-input>
+              <el-input v-model="formData.email"></el-input>
             </el-form-item>
             <el-form-item label="Pass">
-              <el-input v-model="formData.passward"></el-input>
+              <el-input v-model="formData.password"></el-input>
             </el-form-item>
             <el-form-item label="">
-              <el-button type="primary">send</el-button>
+              <el-button type="primary" @click="postLoginData">send</el-button>
             </el-form-item>
           </el-form>
         </el-card>
@@ -22,15 +22,40 @@
 </template>
 
 <script>
+import axios from 'axios';
+import { WEB_API_URL } from './../../.env';
+
 export default {
   name: 'login',
   data() {
     return {
       formData: {
         email: '',
-        passward: '',
+        password: '',
       },
     };
+  },
+  methods: {
+    postLoginData() {
+      console.log(this.formData.email);
+      console.log(this.formData.password);
+      axios.post(`${WEB_API_URL}/v1/login`, this.formData)
+            .then((response) => {
+              console.log(response.data);
+            }).catch(() => {
+              this.$confirm('データの取得に失敗しました．再接続しますか？', 'ネットワークエラー', {
+                confirmButtonText: 'OK',
+                cancelButtonText: 'Cancel',
+                type: 'warning',
+              }).then(() => {
+              }).catch(() => {
+                this.$message({
+                  type: 'info',
+                  message: 'Retrying canceled',
+                });
+              });
+            });
+    },
   },
 };
 </script>
